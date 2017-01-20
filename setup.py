4fd70b8f2465593915800f8c7665dcbfb58576e2
Kenneth Reitz <me@kennethreitz.org>
2017-01-19 19:28:15 -0500

    basics

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+
+from setuptools import setup
+
+try:
+    # Python 3
+    from os import dirname
+except ImportError:
+    # Python 2
+    from os.path import dirname
+
+here = os.path.abspath(dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = [
+    'colorama',
+]
+
+setup(
+    name='colored',
+    version='0.0.1',
+    description='TextUI colors for Python.',
+    long_description=long_description,
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/crayons',
+    py_modules=['crayons'],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+
+    ),
+)
