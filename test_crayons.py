015652a2133d5a5a34abe94f260edb0538a09826
Matthew Peveler <matt.peveler@gmail.com>
2019-09-09 05:57:36 -0300

    Add .travis.yml build for crayons

---
+++ test_crayons.py
@@ -0,0 +1,14 @@
+"""Simple test of just running the README example. Better tests to come."""
+
+import crayons
+
+print(crayons.red('red string'))
+print('{} white {}'.format(crayons.red('red'), crayons.blue('blue')))
+crayons.disable()
+print('{} white {}'.format(crayons.red('red'), crayons.blue('blue')))
+crayons.DISABLE_COLOR = False
+print('{} white {}'.format(crayons.red('red'), crayons.blue('blue')))
+print(crayons.red('red string', bold=True))
+print(crayons.yellow('yellow string', bold=True))
+print(crayons.magenta('magenta string', bold=True))
+print(crayons.white('white string', bold=True))
